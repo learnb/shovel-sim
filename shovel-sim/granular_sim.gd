69574adc933f4bdd52e6eded45158a1960f9c248
learn-bryan <learn.bryan@gmail.com>
2025-12-08 19:34:21 -0500

    add initial grid update rules

--- shovel-sim/granular_sim.gd
+++ shovel-sim/granular_sim.gd
@@ -7,6 +7,7 @@ const FLOW_RATE = 0.5	# fraction of excess height to move
 const grid_width = 128
 const grid_height = 128
 var grid_size = grid_height * grid_width
+var update_indices = []
 
 var cell_type : PackedInt32Array
 var cell_height : PackedFloat32Array
@@ -15,17 +16,25 @@ func _ready():
 	cell_type.resize(grid_size)
 	cell_height.resize(grid_size)
 	
+	update_indices.resize(grid_size)
+	for i in range(grid_size):
+		update_indices[i] = i
+	
 	for i in grid_size:
-		cell_type[i] = CellType.EMPTY
-		cell_height[i] = randf_range(0.0, 10.0)
+		cell_type[i] = CellType.SAND
+		cell_height[i] = randf_range(0.0, 50.0)
+
+func _process(_delta):
+	update_grid()
 
 func get_grid():
 	return 
 
 func update_grid():
-	for y in range(grid_height):
-		for x in range(grid_width):
-			process_cell(x, y)
+	# shuffle cell update order
+	update_indices.shuffle()
+	for i in update_indices:
+		process_cell(i % grid_width, i / grid_width)
 
 func idx(x, y):
 	return x + y * grid_width
@@ -44,7 +53,11 @@ func process_cell(x, y):
 	if t != CellType.SAND:
 		return
 	
-	# Check neighbors
+	#cell_height[i] = randf_range(0.0, 10.0)
+	
+	# try spreading to neighbors
+	# TODO: randomize how we iterate the neighbors to "balance" 
+	# 		preference for spreading
 	for off in get_neighbors():
 		var nx = x + off.x
 		var ny = y + off.y
@@ -57,8 +70,18 @@ func process_cell(x, y):
 		
 		var diff = h - nh
 		
+		# only move material if slope exceeds angle of repose
 		if diff > REPOSE:
-			pass
+			var excess = diff - REPOSE
+			var move_amount = excess * FLOW_RATE
+			
+			cell_height[i] -= move_amount
+			cell_height[ni] += move_amount
+			
+			h -= move_amount
+			
+			if h <= 0.0:
+				break
 
 func get_type(x, y):
 	return cell_type[idx(x,y)]
@@ -68,16 +91,13 @@ func set_type(x, y, t):
 
 func get_neighbors():
 	return [
-		Vector2i(-1, 0),	# left
-		Vector2i(1, 0),	# right
-		Vector2i(0, -1),	# down
-		Vector2i(0, 1),	# up
+		Vector2i(-1, 0), # left
+		Vector2i(1, 0),  # right
+		Vector2i(0, -1), # down
+		Vector2i(0, 1),  # up
 		
-		Vector2i(-1, -1),	# down-left
-		Vector2i(1, -1),	# down-right
-		Vector2i(-1, 1),	# up-left
-		Vector2i(1, 1),	# up-right
+		Vector2i(-1, -1), # down-left
+		Vector2i(1, -1),  # down-right
+		Vector2i(-1, 1),  # up-left
+		Vector2i(1, 1),   # up-right
 	]
-
-	
-	
